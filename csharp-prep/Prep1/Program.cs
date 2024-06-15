8b1ad65a866ce8aa75d364437ecbe927edd518bf
reXatrom <ajstylejoshua2837@gmail.com>
2024-06-15 08:07:43 +0100

    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First name? ");
+        string Firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string Lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Lastname}, {Firstname} {Lastname}.");
     }
 }
